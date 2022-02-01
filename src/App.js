94b02e57e334dbd215df3c9a7bd3fbc221d1d09c
rachel-wong <hello.rachelwong@gmail.com>
2022-02-01 18:17:41 +1000

    feat: enable flipp

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import SingleCard  from './components/SingleCard';
 
 const cardImages = [
-  { "src": "/img/helmet-1.png" },
-  {"src": "/img/potion-1.png"},
-  {"src": "/img/ring-1.png"},
-  {"src": "/img/scroll-1.png"},
-  {"src": "/img/shield-1.png"},
-  {"src": "/img/sword-1.png"},
+  { "src": "/img/helmet-1.png", matched: false},
+  {"src": "/img/potion-1.png", matched: false},
+  {"src": "/img/ring-1.png", matched: false},
+  {"src": "/img/scroll-1.png", matched: false},
+  {"src": "/img/shield-1.png", matched: false},
+  {"src": "/img/sword-1.png", matched: false},
 ]
 
 function App() {
   const [cards, setCards] = useState([]);
   const [turns, setTurns] = useState(0);
+
+  // track first and second choices that user makes
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
 
   //shuffle cards when new game button is pressed
   const shuffleCards = () => {
@@ -23,13 +27,45 @@ function App() {
     setCards(shuffledCards);
     setTurns(0);
   }
+
+  // handle a choice
+  const handleChoice = (card) => {
+    choiceOne != null ? setChoiceTwo(card) : setChoiceOne(card);
+  }
+
+  const resetTurn = () => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns(prevTurns => prevTurns + 1);
+  }
+
+  // compare choices
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      if (choiceOne.src === choiceTwo.src) {
+        setCards(prevCards => {
+          return prevCards.map(card => {
+            if (card.src === choiceOne.src) {
+              return { ...card, matched: true }
+            } else {
+              return card
+            }
+          })
+        })
+        resetTurn();
+      } else {
+        setTimeout(() => resetTurn(), 1000) // gives small delay before flipping b ack
+      }
+    }
+  }, [choiceOne, choiceTwo]);
+
   return (
     <div className="App">
       <h1>Memory Game</h1>
       <button onClick={shuffleCards}>New Game</button>
       <div className="card-grid">
         {cards.map((card) => (
-          <SingleCard card={card} key={ card.id}/>
+          <SingleCard card={card} key={card.id} handleChoice={handleChoice} flipped={card === choiceOne || card === choiceTwo || card.matched }/>
         ))}
       </div>
     </div>
--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './SingleCard.css';
 
-const SingleCard = ({ card }) => {
-  return  <div className="card">
-            <img src={card.src} className="front" alt="card front" />
-            <img src="/img/cover.png" alt="card back" className="back" />
-          </div>
+const SingleCard = ({ card, handleChoice, flipped }) => {
+
+  const handleClick = () => {
+    handleChoice(card)
+  }
+
+  return (
+    <div className="card">
+      <div className={ flipped ? "flipped" : ""}>
+        <img src={card.src} className="front" alt="card front" />
+        <img src="/img/cover.png" alt="card back" onClick={handleClick} className="back" />
+      </div>
+    </div>
+  )
 ;
 };
 
